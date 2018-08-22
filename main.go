5358a40172d3b534ee7a5bfaeb92d37f10261ccf
Daved <daved@codemodus.com>
2018-08-22 08:02:32 -0700

    Move trip util func to main file.

--- main.go
+++ main.go
@@ -51,3 +51,15 @@ func run() error {
 
 	return nil
 }
+
+func tripCheckString(err error, value, name string) error {
+	if err != nil {
+		return err
+	}
+
+	if value == "" {
+		err = fmt.Errorf("%q cannot be empty", name)
+	}
+
+	return err
+}
