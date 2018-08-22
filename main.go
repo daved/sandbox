03ec38c68e2a3d3f521acb8f85e6a86f1b4fbc47
Daved <daved@codemodus.com>
2018-08-21 20:48:40 -0700

    Add/Correct exit code handling.

--- main.go
+++ main.go
@@ -7,6 +7,13 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
 	var (
 		dbuser = ""
 		dbpass = ""
@@ -30,18 +37,17 @@ func main() {
 	err = tripCheckString(err, dbpass, "dbpass")
 	err = tripCheckString(err, dbname, "dbname")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "bad configuration: %s\n", err)
-		return
+		return fmt.Errorf("bad configuration: %s", err)
 	}
 
 	db, err := newDataBase(dbuser, dbpass, dbname)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot create new database: %s\n", err)
-		return
+		return fmt.Errorf("cannot create new database: %s", err)
 	}
 
 	if err = db.migrate(migdir, models...); err != nil {
-		fmt.Fprintf(os.Stderr, "cannot migrate database: %s\n", err)
-		return
+		return fmt.Errorf("cannot migrate database: %s", err)
 	}
+
+	return nil
 }
