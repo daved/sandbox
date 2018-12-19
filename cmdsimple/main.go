07ad8c54d466c05f33eda31583e009c61a83a80f
Daved <daved@codemodus.com>
2018-12-19 12:49:59 -0800

    Add simple cmd.

---
+++ cmdsimple/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/codemodus/clip/clifs"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	var (
+		program = path.Base(os.Args[0])
+		verbose bool
+		example string
+	)
+
+	fs := flag.NewFlagSet("global", clifs.FlagErrorHandling)
+	fs.BoolVar(&verbose, "v", verbose, "verbosity")
+	fs.StringVar(&example, "example", example, "example")
+
+	if err := clifs.Parse(fs, os.Args[1:]); err != nil {
+		return clifs.Usage(program, fs, "", err)
+	}
+
+	fmt.Println(verbose, example)
+	return nil
+}
